d06754f67c638473896d1783dc2f14490d34af89
shawn.huang <shawncc.huang@gmail.com>
2018-07-06 15:22:13 +0800

    Initial commit

---
+++ Source/OpenCVTesting/OpenCVTestingGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OpenCVTestingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENCVTESTING_API AOpenCVTestingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
